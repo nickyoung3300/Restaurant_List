0175c81b07c7c2c79db999463334a60f834930ad
nick <nickyoung3300@gmail.com>
2022-01-05 19:03:53 +0800

    ReadMe&Annotation

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
-# Project Title
-
-
-
+# Restaurant List
+ 用Node.js 執行環境 + Express 框架 打造出可供使用者探索美食平台
+<img src="./RestaurantList - homePage.png"><br>
+美食平台首頁
+<img src="./RestaurantList - show.png"><br>
+美食平台 餐廳詳細資訊
+ 
 ## Getting Started
 
+1. 由Github 載入專案
 
-
-
-### Installing
-
-
+2. terminal  cd 到 專案路徑
+```
+npm i install 
+```
+3. 執行 package.json 檔 後 即可用 nodemon 開啟
+```
+nodemon app.js
+```
 
--- app.js
+++ app.js
@@ -1,43 +1,68 @@
+// 引入 express 套件
+
 const express = require('express')
+
+//啟用 express
 
 const app = express()
 
+// 設 port 為 3000
+
 const port = 3000
 
+//引入 express-handlebars
+
 const exphbs = require('express-handlebars')
+
+//設定 layout
 
 app.engine('handlebars',exphbs({defaultLayout:'main'}))
 
 app.set('view engine','handlebars')
 
+//引入 public 中的Css
+
 app.use(express.static('public'))
 
+//引入JSON資料
+
 const restaurantList = require('./restaurant.json')
+
+//首頁
 
 app.get('/',(req,res)=>{
 
     const restaurantList_index = restaurantList.results
-    console.log(restaurantList_index)
+    //console.log(restaurantList_index)
     res.render('index',{restaurantList_index:restaurantList_index})
 })
 
+//Restaurant Detail
+
 app.get('/restaurants/:restaurant_id',(req,res)=>{
-console.log(req.params.restaurant_id)
 
-const restaurantShow = restaurantList.results.find(item => item.id == req.params.restaurant_id)
+    //console.log(req.params.restaurant_id)
 
-console.log(restaurantShow.id)
-console.log(restaurantShow.name)
+const restaurantShow = restaurantList.results.find(item => item.id.toString() === req.params.restaurant_id.toString())
+
+    //console.log(restaurantShow.id)
+    //console.log(restaurantShow.name)
+
 
 res.render('show',{restaurant_show:restaurantShow})
 })
 
+//搜尋路由
+
 app.get('/search',(req,res)=>{
 
-    const restaurantSearch = restaurantList.results.filter(item => item.name.toLowerCase().includes(req.query.keyword.toLowerCase()))
-    console.log(restaurantSearch)
-    res.render('index',{restaurantList_index:restaurantSearch})
+    const keyword = req.query.keyword
+    const restaurantSearch = restaurantList.results.filter(item => item.name.toLowerCase().includes(keyword.toLowerCase()))
+    //console.log(restaurantSearch)
+    res.render('index',{restaurantList_index:restaurantSearch,keyword:keyword})
 })
+
+//啟用伺服器
 
 app.listen(port,()=>{
 console.log('server')
