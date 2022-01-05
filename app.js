d9d5862624f70aa942f31ec9d7cc4d4c58ef2918
nick <nickyoung3300@gmail.com>
2022-01-05 17:10:35 +0800

    initialize environment_1

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars',exphbs({defaultLayout:'main'}))
+
+app.set('view engine','handlebars')
+
+app.use(express.static('public'))
+
+const restaurantList = require('./restaurant.json')
+
+app.get('/',(req,res)=>{
+
+    const restaurantList_index = restaurantList.results
+    console.log(restaurantList_index)
+    res.render('index',{restaurantList_index:restaurantList_index})
+})
+
+app.get('/restaurants/:restaurant_id',(req,res)=>{
+console.log(req.params.restaurant_id)
+
+const restaurantShow = restaurantList.results.find(item => item.id == req.params.restaurant_id)
+
+console.log(restaurantShow.id)
+console.log(restaurantShow.name)
+
+res.render('show',{restaurant_show:restaurantShow})
+})
+
+app.listen(port,()=>{
+console.log('server')
+})
